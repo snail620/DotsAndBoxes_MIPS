50f6f6c4508d76f25f0daa3b4dd32dc561380aeb
jyin <jasper.yin.7@gmail.com>
2023-04-16 20:04:37 -0500

    updated board to use letters for rows and numbers for columns instead

--- gamelogic.asm
+++ gamelogic.asm
@@ -1,56 +1,48 @@
 		.text
 		.globl 	u_turn
-u_turn:		la	$a0, u_turn_msg		#gets users move
+u_turn:		la	$a0, u_turn_msg		# gets users move
 		li	$v0, 4
 		syscall
 		li	$v0, 12
 		syscall
-		move	$s0, $v0
+		move	$s0, $v0		# store row in $s0
 		li	$v0, 5
 		syscall
-		move	$s1, $v0
+		move	$s1, $v0		# store col in $s1
+		sub	$s0, $s0, $s2		# convert from row letter into row number
+						# col_bias is still saved in $s2
 		
-		lw	$t0, col_bias		#convert column char into int
-						#make case insensitive?
-		sub	$s0, $s0, $t0		#use subu?
-		
-		li	$v0, 4			#check for valid input
+		li	$v0, 4			# check for valid input
 		li	$t0, 15
-		bltu	$s0, $t0, valid_col	#branch if column is valid (in range)
+		bltu	$s1, $t0, valid_col	# branch if column is valid (in range)
 		la	$a0, oor_col
 		syscall
 		j	u_turn
 valid_col:	li	$t0, 11
-		bltu	$s1, $t0, valid_row	#branch if row is valid (in range)
+		bltu	$s0, $t0, valid_row	# branch if row is valid (in range)
 		la	$a0, oor_row
 		syscall
 		j	u_turn
-valid_row:	andi	$t0, $s0, 1		#get LSB of row and col to determine parity
+valid_row:	andi	$t0, $s0, 1		# get LSB of row and col to determine parity
 		andi	$t1, $s1, 1
-		bne	$t0, $t1, valid_space	#branch if space is valid (parities of row and col are different)
+		bne	$t0, $t1, valid_space	# branch if space is valid (parities of row and col are different)
 		la	$a0, inv_space
 		syscall
 		j	u_turn
-valid_space:	sll	$t2, $s1, 4		#calculate displacement address of byte to update
-		add	$t2, $t2, $s0
+valid_space:	sll	$t2, $s0, 4		# calculate displacement address of byte to update
+		add	$t2, $t2, $s1
 		lbu	$t3, board($t2)
 		lbu	$t4, space
-		beq	$t3, $t4, empty_space	#branch if space to place line is currently empty
+		beq	$t3, $t4, empty_space	# branch if space to place line is currently empty
 		la	$a0, occ_space
 		syscall
 		j	u_turn
-empty_space:	beq	$t1, $zero, even_row
+empty_space:	beq	$t0, $zero, even_row
 		lbu	$t3, ver_line
 		j	update_board
 even_row:	lbu	$t3, hor_line
-update_board:	sb	$t3, board($t2)		#update byte at appropriate address
-		#li	$t4, 1			#commented out for debugging, restore later
-		#sw	$t4, turn
-		j	main
-		
-end_debug:	li	$v0, 10
-		syscall		
-										
+update_board:	sb	$t3, board($t2)		# update byte at appropriate address
+									
 return_main:	j	main
 		
 		.data
@@ -60,9 +52,7 @@ oor_row:	.asciiz "Selected row is out of range! Make sure your row number comes 
 inv_space:	.asciiz "You can't place a line here because it is a dot or the center of a box! Try again.\n"
 occ_space:	.asciiz "You can't place a line here because it already has a line! Try again.\n"
 		.align	2
-col_bias:	.ascii	"A"			#probably a better way to do this?
-		.align	2
-hor_line:	.ascii	"-"
+hor_line:	.ascii	"-"			# probably a better way to do this?
 		.align	2
 ver_line:	.ascii	"|"
 		.align 2
